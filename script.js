dda1befab8f087f1406f332fa9f1557b24485049
shafali03 <shaf03sm@gmail.com>
2019-05-08 20:03:06 +0100

    update

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
 const cartDOM = document.querySelector('.cart');
 const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items')
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
@@ -67,8 +68,6 @@ class UI {
     getBagButtons() {
         //turn to array
         const buttons = [...document.querySelectorAll('.bag-btn')];
-
-
         buttonsDOM = buttons;
         buttons.forEach(button => {
             let id = button.dataset.id;
@@ -76,26 +75,47 @@ class UI {
             let inCart = cart.find(item => item.id === id);
             if (inCart) {
                 button.innerText = "In Cart";
-                button.disable = true;
+                button.disabled = true;
             }
-            button.addEventListener('click', (event) => {
+            button.addEventListener('click', event => {
                 // console.log(event);
                 event.target.innerText = "In Cart";
                 event.target.disabled = true;
                 //get product from products
-                let cartItem = { ...Storage.getProducts(id), amount: 1 };
+                let cartItem = { ...Storage.getProduct(id), amount: 1 };
 
                 //add product to cart
                 cart = [...cart, cartItem];
 
                 //save cart in local storage
-                Storage.saveCart(cart)
+                Storage.saveCart(cart);
+
                 //set cart values
+                this.setCartValues(cart);
                 //display cart item
+                this.addCartItem(cartItem)
                 //show the cart
             });
         });
     }
+
+    setCartValues(cart) {
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        });
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+    }
+    addCartItem(item) {
+        const div = document.createElement('div');
+    }
+
+
+
+
 }
 
 // local storage
@@ -103,7 +123,7 @@ class Storage {
     static saveProducts(products) {
         localStorage.setItem("products", JSON.stringify(products));
     }
-    static getProducts(id) {
+    static getProduct(id) {
         let products = JSON.parse(localStorage.getItem('products'))
         //return array in local storage
         return products.find(product => product.id === id)
